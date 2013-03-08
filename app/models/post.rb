2d10310901a57e75738dcef469a7655c388da577
naomimatsuura <matsuura.naomi@gmail.com>
2013-03-07 21:07:48 -0500

    Validations and associations for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :description, :link
+
+  validates :description, presence: true
+  validates :link, presence: true,
+  				   format: /^http:\/\/.+?/
+
+  has_many :comments
+
+  				   #http://facebook.com
 end
