77c99dce22f58e88b6c96349f6b0deab5798398a
naomimatsuura <matsuura.naomi@gmail.com>
2013-04-02 21:08:03 -0400

    added tests in post_test.rb

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,6 +1,23 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+
+	def valid_attributes
+		{ description: "Test", link: "http://www.google.com" }
+	end
+
+	test "posts without users should not be valid" do
+		p = Post.new(valid_attributes)
+		p.user = nil
+		#assert_false p.valid?
+		assert_equal false, p.valid?
+	end
+
+	test "posts with users should be valid" do
+		p = Post.new(valid_attributes)
+		p.user = User.new
+		assert p.valid?, p.errors.full_messages.join(", ")
+	end
   # test "the truth" do
   #   assert true
   # end
